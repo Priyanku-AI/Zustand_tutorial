462d6c55224e3727781fb9dd860f1bb3adbe1188
Priyanku Saikia <123909182+Priyanku-AI@users.noreply.github.com>
2025-06-17 04:31:10 +0530

    port changed to show side-by-side comparison

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { port: 5175 }, // For zustand
   plugins: [react(), tailwindcss()],
 })
